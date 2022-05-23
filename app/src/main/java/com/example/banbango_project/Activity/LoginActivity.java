8fe8018a17bd4fad20e91c9be6f204f0edca4b3d
Raven-PC\Raven <dragondraven51@gmail.com>
2022-05-23 21:22:15 +0700

    Login Google Belum Coba

--- app/src/main/java/com/example/banbango_project/Activity/LoginActivity.java
+++ app/src/main/java/com/example/banbango_project/Activity/LoginActivity.java
@@ -1,5 +1,7 @@
 package com.example.banbango_project.Activity;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
@@ -15,6 +17,18 @@ import android.widget.Toast;
 import com.example.banbango_project.API.BaseAPIServer;
 import com.example.banbango_project.API.UtilsApi;
 import com.example.banbango_project.R;
+import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+import com.google.android.gms.auth.api.identity.Identity;
+import com.google.android.gms.auth.api.identity.SignInClient;
+import com.google.android.gms.auth.api.identity.SignInCredential;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -27,6 +41,7 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class LoginActivity extends AppCompatActivity {
+    private static final String TAG = "";
     EditText EditEnterEmail, EditEnterPass;
     Button btnLogin;
     ProgressDialog loading;
@@ -34,14 +49,35 @@ public class LoginActivity extends AppCompatActivity {
     Context context;
     BaseAPIServer apiServer;
 
+    private SignInClient oneTapClient;
+    private BeginSignInRequest.Builder signInRequest;
+    private FirebaseAuth auth;
+
+    private static final int REQ_ONE_TAP = 2;
+    private boolean showOneTapUI = true;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        auth = FirebaseAuth.getInstance();
+        oneTapClient = Identity.getSignInClient(this);
+        signInRequest = BeginSignInRequest.builder().setGoogleIdTokenRequestOptions(BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                .setSupported(true)
+                .setServerClientId(getString(R.string.web_client_id))
+                .setFilterByAuthorizedAccounts(true)
+                .build());
+
         context = this;
         apiServer = UtilsApi.getApiService();
         initComponenets();
+    }
+    public void signOut() {
+        // [START auth_sign_out]
+        FirebaseAuth.getInstance().signOut();
+        // [END auth_sign_out]
     }
 
     private void initComponenets() {
@@ -61,11 +97,11 @@ public class LoginActivity extends AppCompatActivity {
                         .enqueue(new Callback<ResponseBody>() {
                             @Override
                             public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                                if (response.isSuccessful()){
+                                if (response.isSuccessful()) {
                                     loading.dismiss();
                                     try {
                                         JSONObject jsonObject = new JSONObject(response.body().string());
-                                        if (jsonObject.getString("error").equals("false")){
+                                        if (jsonObject.getString("error").equals("false")) {
                                             // jika login berhasil maka data nama yang ada di response API
                                             // akan diparsing ke activing selanjutnya
                                             Toast.makeText(context, "Succsesfully Login", Toast.LENGTH_SHORT).show();
@@ -78,12 +114,12 @@ public class LoginActivity extends AppCompatActivity {
                                             String error_message = jsonObject.getString("error_msg");
                                             Toast.makeText(context, error_message, Toast.LENGTH_SHORT).show();
                                         }
-                                    } catch (JSONException e){
+                                    } catch (JSONException e) {
                                         e.printStackTrace();
                                     } catch (IOException e) {
                                         e.printStackTrace();
                                     }
-                                }else {
+                                } else {
                                     loading.dismiss();
                                 }
                             }
@@ -97,4 +133,54 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_ONE_TAP:
+                try {
+                    SignInCredential credential = oneTapClient.getSignInCredentialFromIntent(data);
+                    String idToken = credential.getGoogleIdToken();
+                    if (idToken != null) {
+                        // Got an ID token from Google. Use it to authenticate
+                        // with Firebase.
+                        AuthCredential firebaseCredential = GoogleAuthProvider.getCredential(idToken, null);
+                        auth.signInWithCredential(firebaseCredential)
+                                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        if (task.isSuccessful()) {
+                                            // Sign in success, update UI with the signed-in user's information
+                                            Log.d(TAG, "signInWithCredential:success");
+                                            FirebaseUser user = auth.getCurrentUser();
+                                            updateUI(user);
+                                        } else {
+                                            // If sign in fails, display a message to the user.
+                                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                                            updateUI(null);
+                                        }
+                                    }
+                                });
+                    }
+                } catch (ApiException e) {
+                    // ...
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = auth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    private void updateUI(FirebaseUser user) {
+
+    }
 }
+
