76c838eed29b4fa554557ffd811cfee34a3981f1
Raven-PC\Raven <dragondraven51@gmail.com>
2022-05-27 11:37:08 +0700

    Cari Lokasi

---
+++ app/src/main/java/com/example/banbango_project/Activity/CariLokasiActivity.java
@@ -0,0 +1,16 @@
+package com.example.banbango_project.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.banbango_project.R;
+
+public class CariLokasiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cari_lokasi);
+    }
+}
