186ff6a0cea45729c2b83e3cd2fe8d42502a600a
DESKTOP-KR575NH\Suci Khairatuz Zahra <sucikhairatuzzahra13@gmail.com>
2022-07-02 16:53:57 +0700

    ada tambahan 4 activity baru dan 3 xml baru

---
+++ app/src/main/java/com/example/banbango_project/Activity/ProvinsiActivity.kt
@@ -0,0 +1,11 @@
+package com.example.banbango_project.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProvinsiActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_provinsi)
+    }
+}
